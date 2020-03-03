c2b2d871b53e6e843f35d2471fdd369d20ed661b
ElectricSigh <61737023+ElectricSigh@users.noreply.github.com>
2020-03-03 12:28:29 +0100

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+# Questo è un commento
+"""
+Questo è un altro commento: 
+non essendo richiamato in una variabile 
+rimane un commento ma questa scrittura può
+essere intesa anche come una stringa in una 
+assegnazione
+"""
